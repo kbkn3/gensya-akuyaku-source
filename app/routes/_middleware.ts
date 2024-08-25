530b769d25f8bf3afcecfbc4a6919c06e63a449d
Wabu-K <KobayashiKenta@lifull.com>
2024-08-25 10:13:44 +0900

    feat: Update secure headers and add nonce to client script
    Enhance security by updating the secure headers in the _middleware.ts file to include the strictTransportSecurity and contentSecurityPolicy headers. Additionally, add a nonce attribute to the client script in the _renderer.tsx file to improve security measures.

--- app/routes/_middleware.ts
+++ app/routes/_middleware.ts
@@ -4,19 +4,16 @@ import { logger } from 'hono/logger'
 
 export default createRoute(
   logger(),
-  secureHeaders(
-    //   {
-    //   strictTransportSecurity:
-    //     'max-age=63072000; includeSubDomains; preload',
-    //   contentSecurityPolicy: import.meta.env.PROD
-    //     ? {
-    //         scriptSrc: [NONCE],
-    //         defaultSrc: ["'self'"],
-    //       }
-    //     : undefined,
-    // }
-  ),
-  async c => {
-    return c.res.headers.append('Permissions-Policy', '')
-  },
+  secureHeaders({
+    strictTransportSecurity: 'max-age=63072000; includeSubDomains; preload',
+    contentSecurityPolicy: import.meta.env.PROD
+      ? {
+          scriptSrc: [NONCE],
+          defaultSrc: ["'self'"],
+        }
+      : undefined,
+  }),
+  // async c => {
+  //   return c.res.headers.append('Permissions-Policy', 'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()')
+  // },
 )
