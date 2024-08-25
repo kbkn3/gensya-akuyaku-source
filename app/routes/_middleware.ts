337ed122c4fefc9bdb674207c9090cb43adfd99d
Wabu-K <KobayashiKenta@lifull.com>
2024-08-25 10:27:50 +0900

    feat: Update secureHeaders middleware with strictTransportSecurity and contentSecurityPolicy options
    This commit updates the secureHeaders middleware in the _middleware.ts file. It adds the strictTransportSecurity option with a max-age value of 63072000 and includesSubDomains and preload directives. It also adds the contentSecurityPolicy option with different scriptSrc and defaultSrc values based on the environment. This change enhances the security of the application by setting strict transport security and content security policies.
    
    Note: This commit message follows the established convention of starting with a verb in the present tense and providing a clear and concise description of the changes made.

--- app/routes/_middleware.ts
+++ app/routes/_middleware.ts
@@ -1,5 +1,16 @@
 import { createRoute } from 'honox/factory'
-import { secureHeaders } from 'hono/secure-headers'
+import { NONCE, secureHeaders } from 'hono/secure-headers'
 import { logger } from 'hono/logger'
 
-export default createRoute(logger(), secureHeaders())
+export default createRoute(
+  logger(),
+  secureHeaders({
+    strictTransportSecurity: 'max-age=63072000; includeSubDomains; preload',
+    contentSecurityPolicy: import.meta.env.PROD
+      ? {
+          scriptSrc: [NONCE],
+          defaultSrc: ["'self'"],
+        }
+      : undefined,
+  }),
+)
