e2c828f8da956e243f18b75418c6f1f98803e9b8
Wabu-K <KobayashiKenta@lifull.com>
2024-08-25 10:28:46 +0900

    feat: Add Permissions-Policy header to _middleware.ts
    Add the Permissions-Policy header to the _middleware.ts file. This header sets the permissions policy for various browser features, such as accelerometer, ambient-light-sensor, autoplay, battery, camera, and more. This change enhances the security and privacy of the application by controlling the access to these features.
    
    Note: This commit message follows the established convention of starting with a verb in the present tense and providing a clear and concise description of the changes made.

--- app/routes/_middleware.ts
+++ app/routes/_middleware.ts
@@ -13,4 +13,10 @@ export default createRoute(
         }
       : undefined,
   }),
+  async c => {
+    return c.res.headers.append(
+      'Permissions-Policy',
+      'accelerometer=(), ambient-light-sensor=(), autoplay=(), battery=(), camera=(), cross-origin-isolated=(), display-capture=(), document-domain=(), encrypted-media=(), execution-while-not-rendered=(), execution-while-out-of-viewport=(), fullscreen=(), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), navigation-override=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(), usb=(), web-share=(), xr-spatial-tracking=()',
+    )
+  },
 )
